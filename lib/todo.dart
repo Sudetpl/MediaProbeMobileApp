a6dbc22315d7c14073294842099e417ceb57ed97
Sude Topal <52575122+Sudetpl@users.noreply.github.com>
2023-07-18 10:18:37 +0300

    Update todo.dart

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,8 +1,1 @@
-/*
-Localization, +
-Serviste son 7 günü sen elle vermişsin. dışarıdan verilecek şekilde ayarla. configde kaç günlük alınacağına dair bir int tut . onu daha sonra servisdeki fonsksiyon çağrılırken kullan. bu sayede configden dinamik değişen bir yapıda olacak.!!!!! +++
-Unit Test,
 
-
-
-*/
